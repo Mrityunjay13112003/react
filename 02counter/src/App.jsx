165c3e8ed97ed64f517bf34666997322dd679bd0
Mrityunjay Tripathi <mrityunjaytripathi596@gmail.com>
2026-05-27 16:02:45 +0530

    initialize counter app with React and Vite and used useState hook of react to create a button for increasing value and another button for decreasing the value displayed on UI

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,40 @@
+import React, {useState} from 'react'; 
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0);
+
+  const addvalue = () => {
+    
+    if(counter < 20)
+    {
+      setCounter(++counter);
+      console.log("value increased", counter);
+    }
+    
+  }
+
+  const decreaseValue = () => {
+    
+    if(counter > 0)
+    {
+      setCounter(--counter);
+      console.log("value decreased", counter);
+    }
+    
+  }
+
+  return (
+    <>
+      <h1>Chai aur React.</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addvalue}>Add Value</button>
+      <br />
+      <button onClick={decreaseValue}>Remove value</button>
+    </>
+  )
+}
+
+export default App
