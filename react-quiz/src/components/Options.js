bb870ee5ee7ed531b6936deb16cd1df5e92367c9
Mrityunjay Tripathi <mrityunjaytripathi596@gmail.com>
2026-06-26 01:10:00 +0530

    partially completed react-quiz project which is based on the useReducer hook

---
+++ react-quiz/src/components/Options.js
@@ -0,0 +1,16 @@
+export default function Options({ question, dispatch, answer }) {
+  return (
+    <div className="options">
+      {question?.options?.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${answer !== null ? (index === question.correctOption ? "correct" : "wrong") : ""}`}
+          key={option}
+          disabled={answer !== null}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
