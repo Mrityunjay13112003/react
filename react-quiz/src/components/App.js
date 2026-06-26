3404ad1abf0d40ef4cd7ffce6ad9cc1babe1882d
Mrityunjay Tripathi <mrityunjaytripathi596@gmail.com>
2026-06-26 23:11:58 +0530

    Completed the React Quiz App

--- react-quiz/src/components/App.js
+++ react-quiz/src/components/App.js
@@ -6,6 +6,12 @@ import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
+import Footer from "./Footer";
+import Timer from "./Timer";
+
+  const SECS_PER_QUESTION = 30;
 
 const initialState = {
   questions: [],
@@ -15,6 +21,8 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -34,6 +42,7 @@ function reducer(state, action) {
       return {
         ...state,
         status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
       };
     case "newAnswer":
       const question = state.questions.at(state.index);
@@ -48,21 +57,41 @@ function reducer(state, action) {
     case "nextQuestion":
       return {
         ...state,
-        index: state.index < state.questions.length-1 ? state.index + 1 : state.index,
-        answer: state.index < state.questions.length-1 ? null : state.answer,
+        index:
+          state.index < state.questions.length - 1
+            ? state.index + 1
+            : state.index,
+        answer: state.index < state.questions.length - 1 ? null : state.answer,
       };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    case "restart":
+      return { ...initialState, questions: state.questions, status: "ready" };
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? 'finished' : state.status,
+      }
     default:
       throw new Error("Action is unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
-    reducer,
-    initialState,
-  );
+  const [{ questions, status, index, answer, points, highscore, secondsRemaining }, dispatch] =
+    useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, curr) => prev + curr.points,
+    0,
+  );
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -83,13 +112,37 @@ export default function App() {
         )}
         {status === "active" && (
           <>
+            <Progress
+              index={index}
+              numQuestion={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
             <Question
               question={questions[index]}
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton dispatch={dispatch} answer={answer} />
+
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                index={index}
+                numQuestions={numQuestions}
+              />
+            </Footer>
           </>
+        )}
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+            dispatch={dispatch}
+          />
         )}
       </Main>
     </div>
---
+++ react-quiz/src/components/Progress.js
@@ -0,0 +1,15 @@
+export default function Progress({ index, numQuestion, points, maxPossiblePoints, answer }) {
+  return (
+    <div>
+      <header className="progress">
+        <progress max={numQuestion} value={index + Number(answer !== null)} />
+
+        <p>
+          Question <strong>{index + 1}</strong> / {numQuestion}
+        </p>
+
+        <p><strong>{points} / {maxPossiblePoints}</strong></p>
+      </header>
+    </div>
+  );
+}
