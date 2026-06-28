d9df9e7594611799ba4d44cc15b7c1d0c5f53508
Mrityunjay Tripathi <mrityunjaytripathi596@gmail.com>
2026-06-28 23:43:10 +0530

    Partially completed the world wise react project

--- worldwise/src/components/CityList.jsx
+++ worldwise/src/components/CityList.jsx
@@ -1,9 +1,26 @@
-import styles from './CityList.module.css'
+/* eslint-disable react/prop-types */
+import Spinner from "./Spinner";
+import styles from "./CityList.module.css";
+import CityItem from "./CityItem";
+import Message from "./Message"; 
 
-function CityList() {
+function CityList({ cities, isLoading }) {
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if(!cities.length)
+  {
+    return <Message message="Add your first city by clicking on a city on the map" />
+  }
+
   return (
-    <ul className={styles.cityList}>List</ul>
-  )
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
 }
 
-export default CityList
+export default CityList;
